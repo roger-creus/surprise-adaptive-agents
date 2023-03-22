06fd1a4364f4662704d0404b092e148a9736eb8e
Adriana Hugessen <hugessen.adriana@gmail.com>
2023-03-21 21:08:07 -0400

    comet logging (#2)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-    name='smirl',
+    name='surprise_adapt',
     version='0.1.0',
     packages=find_packages(),
     long_description=open('README.md').read(),
