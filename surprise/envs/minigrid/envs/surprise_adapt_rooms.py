a5b80438de25cf73015c39a91032dd6b6c590453
Adriana Hugessen <hugessen.adriana@gmail.com>
2023-04-14 11:51:52 -0400

    variable noise and center view

--- surprise/envs/minigrid/envs/surprise_adapt_rooms.py
+++ surprise/envs/minigrid/envs/surprise_adapt_rooms.py
@@ -55,7 +55,8 @@ class SurpriseAdaptRoomsEnv(MiniGridEnv):
     - `MiniGrid-FourRooms-v0`
     """
 
-    def __init__(self, max_steps=100, noisy_room=2, num_doors=1, **kwargs):
+    def __init__(self, max_steps=100, noisy_room=2, num_doors=1, noisy_prob=1,
+                 agent_view="front", **kwargs):
         self._agent_default_pos = None
         self._goal_default_pos = None
 
@@ -65,7 +66,9 @@ class SurpriseAdaptRoomsEnv(MiniGridEnv):
         self.width = (self.room_size + 1) * self.num_rooms + 1
         assert noisy_room in (1, 2), "Please select one of (1,2) as the noisy room"
         self.noisy_room = noisy_room
-        self.noisy_floors = None
+        self.noisy_prob = noisy_prob
+
+        self.agent_view = agent_view
         self.num_doors = num_doors
 
         super().__init__(
@@ -82,28 +85,25 @@ class SurpriseAdaptRoomsEnv(MiniGridEnv):
         return "reach the goal"
 
     def step(self, action):
+        self._randomize_floor_colors()
         obs, reward, done, info = super().step(action)
         info.update({'agent_pos': (self.agent_pos, (self.width, self.height))})
-        self._randomize_floor_colors()
 
         return obs, reward, done, info
 
     def _randomize_floor_colors(self):
         color_names = [color for color in COLOR_NAMES if color != 'grey']
 
-        if self.noisy_floors is None:
-            self.noisy_floors = []
-            for i in range((self.room_size + 1) * (self.noisy_room-1) + 1, (self.room_size + 1) * self.noisy_room):
-                for j in range(1, self.room_size + 1):
+        for i in range((self.room_size + 1) * (self.noisy_room-1) + 1, (self.room_size + 1) * self.noisy_room):
+            for j in range(1, self.room_size + 1):
+                if np.random.uniform() < self.noisy_prob:
                     try:
                         floor_tile = Floor(color=self._rand_elem(color_names))
                         self.put_obj(floor_tile, i, j)
-                        self.noisy_floors.append(floor_tile)
                     except Exception as e:
                         print(f'Could not place noisy floor tile: {e}')
-        else:
-            for floor in self.noisy_floors:
-                floor.color = self._rand_elem(color_names)
+                else:
+                    self.grid.set(i, j, None)
 
     def _gen_grid(self, width, height):
         # Create the grid
@@ -151,6 +151,100 @@ class SurpriseAdaptRoomsEnv(MiniGridEnv):
 
         self.mission = self._gen_mission()
 
-    def reset(self):
-        self.noisy_floors = None
-        return super().reset()
+    def get_view_exts(self):
+        """
+        Get the extents of the square set of tiles visible to the agent
+        Note: the bottom extent indices are not included in the set
+        """
+        if self.agent_view == 'center':
+            topX = self.agent_pos[0] - self.agent_view_size // 2
+            topY = self.agent_pos[1] - self.agent_view_size // 2
+            botX = topX + self.agent_view_size
+            botY = topY + self.agent_view_size
+
+            return (topX, topY, botX, botY)
+        else:
+            return super().get_view_exts()
+
+    def get_obs_render(self, obs, tile_size=TILE_PIXELS//2):
+        """
+        Render an agent observation for visualization
+        """
+
+        grid, vis_mask = Grid.decode(obs)
+
+        if self.agent_view == "center":
+            agent_pos = (self.agent_view_size // 2, self.agent_view_size // 2)
+        else:
+            agent_pos = (self.agent_view_size // 2, self.agent_view_size - 1)
+        # Render the whole grid
+        img = grid.render(
+            tile_size,
+            agent_pos=agent_pos,
+            agent_dir=3,
+            highlight_mask=vis_mask
+        )
+
+        return img
+
+    def render(self, mode='human', close=False, highlight=True, tile_size=TILE_PIXELS):
+        """
+        Render the whole-grid human view
+        """
+
+        if close:
+            if self.window:
+                self.window.close()
+            return
+
+        if mode == 'human' and not self.window:
+            import gym_minigrid.window
+            self.window = gym_minigrid.window.Window('gym_minigrid')
+            self.window.show(block=False)
+
+        # Compute which cells are visible to the agent
+        _, vis_mask = self.gen_obs_grid()
+
+        # Compute the world coordinates of the bottom-left corner
+        # of the agent's view area
+        f_vec = self.dir_vec
+        r_vec = self.right_vec
+        if self.agent_view == "center":
+            top_left = self.agent_pos + f_vec * (self.agent_view_size // 2) - r_vec * (self.agent_view_size // 2)
+        else:
+            top_left = self.agent_pos + f_vec * (self.agent_view_size-1) - r_vec * (self.agent_view_size // 2)
+
+        # Mask of which cells to highlight
+        highlight_mask = np.zeros(shape=(self.width, self.height), dtype=bool)
+
+        # For each cell in the visibility mask
+        for vis_j in range(0, self.agent_view_size):
+            for vis_i in range(0, self.agent_view_size):
+                # If this cell is not visible, don't highlight it
+                if not vis_mask[vis_i, vis_j]:
+                    continue
+
+                # Compute the world coordinates of this cell
+                abs_i, abs_j = top_left - (f_vec * vis_j) + (r_vec * vis_i)
+
+                if abs_i < 0 or abs_i >= self.width:
+                    continue
+                if abs_j < 0 or abs_j >= self.height:
+                    continue
+
+                # Mark this cell to be highlighted
+                highlight_mask[abs_i, abs_j] = True
+
+        # Render the whole grid
+        img = self.grid.render(
+            tile_size,
+            self.agent_pos,
+            self.agent_dir,
+            highlight_mask=highlight_mask if highlight else None
+        )
+
+        if mode == 'human':
+            self.window.set_caption(self.mission)
+            self.window.show_img(img)
+
+        return img
