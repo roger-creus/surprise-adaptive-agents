b722fbed9dcd97a2b90fb8d1904b49cac0d88da7
roger-creus <creus99@gmail.com>
2023-04-21 18:04:31 -0400

    added RandomAlpha switches, FixedLenght Alpha switches, and SA in tetris

--- scripts/dqn_smirl.py
+++ scripts/dqn_smirl.py
@@ -84,7 +84,7 @@ def add_wrappers(env, variant, device=0, eval=False, network=None, flip_alpha=Fa
         if "smirl_wrapper" in wrapper:
             env = add_smirl(env=env, variant=wrapper["smirl_wrapper"], ep_length=variant["env_kwargs"]["max_steps"], device=device)
         elif "surprise_adapt_wrapper" in wrapper:
-            env = add_surprise_adapt(env=env, variant=wrapper["surprise_adapt_wrapper"], ep_length=variant["env_kwargs"]["max_steps"], device=device, flip_alpha=flip_alpha)
+            env = add_surprise_adapt(env=env, variant=wrapper["surprise_adapt_wrapper"], ep_length=variant["env_kwargs"]["max_steps"], device=device, flip_alpha=flip_alpha, flip_alpha_strategy=wrapper["surprise_adapt_wrapper"]["flip_alpha_strategy"])
         elif "soft_reset_wrapper" in wrapper:
             env = SoftResetWrapper(env=env, max_time=variant["env_kwargs"]["max_steps"])
         elif "FlattenObservationWrapper" in wrapper:
@@ -147,7 +147,7 @@ def add_wrappers(env, variant, device=0, eval=False, network=None, flip_alpha=Fa
     return env, network
 
 
-def add_surprise_adapt(env, variant, ep_length = 500, device = 0, flip_alpha=False):
+def add_surprise_adapt(env, variant, ep_length = 500, device = 0, flip_alpha=False, flip_alpha_strategy="SA"):
     from surprise.buffers.buffers import BernoulliBuffer, GaussianBufferIncremental, GaussianCircularBuffer
     from surprise.wrappers.base_surprise_adapt import BaseSurpriseAdaptWrapper
 
@@ -158,7 +158,7 @@ def add_surprise_adapt(env, variant, ep_length = 500, device = 0, flip_alpha=Fal
 
     if (variant["buffer_type"] == "Bernoulli"):
         buffer = BernoulliBuffer(obs_size)
-        env = BaseSurpriseAdaptWrapper(env, buffer, time_horizon=ep_length, flip_alpha=flip_alpha,**variant)
+        env = BaseSurpriseAdaptWrapper(env, buffer, time_horizon=ep_length, flip_alpha=flip_alpha, **variant)
         
     elif (variant["buffer_type"] == "Gaussian"):
         buffer = GaussianBufferIncremental(obs_size)
@@ -246,7 +246,16 @@ def experiment(doodad_config, variant):
     
 #     base_env2 = RenderingObservationWrapper(base_env2)
     expl_env, network = add_wrappers(base_env, variant, device=ptu.device)
-    eval_env, _ = add_wrappers(base_env2, variant, flip_alpha = True, device=ptu.device, eval=True, network=network)
+    
+    # this is only for SA with Fixed Alphas during training to flip alphas according to SA in eval only
+    flip_alpha_eval = False
+    if "surprise_adapt_wrapper" in variant["wrappers"]:
+        window_len = variant["wrappers"]["surprise_adapt_wrapper"]["surprise_window_len"]
+        flip_strategy = variant["wrappers"]["surprise_adapt_wrapper"]["flip_alpha_strategy"]
+        if window_len == -1 and flip_alpha_strategy == "SA":
+            flip_alpha_eval = True
+
+    eval_env, _ = add_wrappers(base_env2, variant, flip_alpha = flip_alpha_eval, device=ptu.device, eval=True, network=network)
     if ("vae_wrapper" in variant["wrappers"]):
         eval_env._network = base_env._network
 
