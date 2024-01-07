962b957452ab658b75ba0204e0aa101d41fdbe73
Faisal <faisal.ahmed.a.m@gmail.com>
2024-01-07 12:43:34 +0000

    wip

--- cluster_scripts/crafter/start_crafter_short.sh
+++ cluster_scripts/crafter/start_crafter_short.sh
@@ -1,10 +1,8 @@
 
 
-# Smin
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN.json --run_mode=local --exp_name=crafter_smin_short  --log_comet true --training_processor_type=gpu
-
-# Smax
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX.json --run_mode=local --exp_name=crafter_smax_short  --log_comet true --training_processor_type=gpu
-
+# # Smin
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN.json --run_mode=local --exp_name=crafter_smin_short  --log_comet true --training_processor_type=gpu
+# # Smax
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX.json --run_mode=local --exp_name=crafter_smax_short  --log_comet true --training_processor_type=gpu
 # Sa
 sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA.json --run_mode=local --exp_name=crafter_sa_short  --log_comet true --training_processor_type=gpu
