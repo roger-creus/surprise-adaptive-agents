addb700d5924785bf989f69912f406ec0c657069
Faisal <faisal.ahmed.a.m@gmail.com>
2024-01-09 17:06:08 +0000

    wip

--- cluster_scripts/crafter/start_crafter_short.sh
+++ cluster_scripts/crafter/start_crafter_short.sh
@@ -7,9 +7,9 @@ sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/craft
 # # Sa
 sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA.json --run_mode=local --exp_name=crafter_sa_short_discounting  --training_processor_type=gpu --log_comet=true
 
-# # Sa long
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_long.json --run_mode=local --exp_name=crafter_sa_long_discounting  --log_comet true --training_processor_type=gpu
-# # Smin long
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_long.json --run_mode=local --exp_name=crafter_smin_long_discounting  --log_comet=true --training_processor_type=gpu
-# Smaxlong
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_long.json --run_mode=local --exp_name=crafter_smax_long_discounting  --log_comet true --training_processor_type=gpu
+# # # Sa long
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_long.json --run_mode=local --exp_name=crafter_sa_long_discounting  --log_comet true --training_processor_type=gpu
+# # # Smin long
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_long.json --run_mode=local --exp_name=crafter_smin_long_discounting  --log_comet=true --training_processor_type=gpu
+# # Smaxlong
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_long.json --run_mode=local --exp_name=crafter_smax_long_discounting  --log_comet true --training_processor_type=gpu
