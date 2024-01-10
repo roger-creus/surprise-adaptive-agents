75ca9cc70ab8cf7c70f74cde0cb72a4a22f7baab
Faisal <faisal.ahmed.a.m@gmail.com>
2024-01-10 09:55:55 +0000

    experimnet with large replay size

--- cluster_scripts/crafter/start_crafter_short.sh
+++ cluster_scripts/crafter/start_crafter_short.sh
@@ -13,3 +13,15 @@ sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/craft
 sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_long.json --run_mode=local --exp_name=crafter_smin_long_discounting  --log_comet=true --training_processor_type=gpu
 # Smaxlong
 sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_long.json --run_mode=local --exp_name=crafter_smax_long_discounting  --log_comet true --training_processor_type=gpu
+
+
+####### Larger replay buffer ############
+
+# Smin
+sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_large_replay_buffer.json --run_mode=local --exp_name=crafter_smin_short_large_replay  --training_processor_type=gpu --log_comet=true
+# Smax
+sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_large_replay_buffer.json --run_mode=local --exp_name=crafter_smax_short_large_replay  --training_processor_type=gpu --log_comet=true
+# Sa
+sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_large_replay_buffer.json --run_mode=local --exp_name=crafter_sa_short_large_replay  --training_processor_type=gpu --log_comet=true
+
+####### Larger replay buffer ############
