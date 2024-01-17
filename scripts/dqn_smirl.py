12ce1285e0329291667e775a749051ca58fecf47
Faisal <faisal.ahmed.a.m@gmail.com>
2024-01-17 09:07:41 +0400

    wip

--- scripts/dqn_smirl.py
+++ scripts/dqn_smirl.py
@@ -272,7 +272,7 @@ def add_wrappers(env, variant, device=0, eval=False, network=None, flip_alpha=Fa
             metrics_save_path = f"{base_path}/{exp_name}/{metric_path}"
             os.makedirs(metrics_save_path, exist_ok=True)
             env = CrafterWrapper(env, save_metrics=True, save_metrics_path=metrics_save_path)
-        elif "rescale_reward" in wrapper:
+        elif "rescale_reward_wrapper" in wrapper:
             print("rescale_reward_wrapper")
             env = RescaleImageWrapper(env)
         else:
