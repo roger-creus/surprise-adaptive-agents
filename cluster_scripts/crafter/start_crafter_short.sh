861fd45df6899ab40a68353a3f126731ac9866aa
Faisal <faisal.ahmed.a.m@gmail.com>
2024-01-18 15:02:49 +0400

    wip

--- cluster_scripts/crafter/start_crafter_short.sh
+++ cluster_scripts/crafter/start_crafter_short.sh
@@ -12,14 +12,14 @@
 # sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_rescale_reward.json --run_mode=local --exp_name=crafter_smax_rescale_reward --training_processor_type=gpu --log_comet=true
 
 # Without softReset
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_no_softreset.json --run_mode=local --exp_name=crafter_sa_no_softreset --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_no_softreset.json --run_mode=local --exp_name=crafter_smin_no_softreset --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_no_softreset.json --run_mode=local --exp_name=crafter_smax_no_softreset --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_no_softreset.json --run_mode=local --exp_name=crafter_sa_no_softreset --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_no_softreset.json --run_mode=local --exp_name=crafter_smin_no_softreset --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_no_softreset.json --run_mode=local --exp_name=crafter_smax_no_softreset --training_processor_type=gpu --log_comet=true
 
-# Without softReset and reward scaling by stf
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_rescale_reward_no_softreset.json --run_mode=local --exp_name=crafter_sa_rescale_reward_no_softreset --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_rescale_reward_no_softreset.json --run_mode=local --exp_name=crafter_smin_rescale_reward_no_softreset --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_rescale_reward_no_softreset.json --run_mode=local --exp_name=crafter_smax_rescale_reward_no_softreset --training_processor_type=gpu --log_comet=true
+# # Without softReset and reward scaling by std
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_rescale_reward_no_softreset.json --run_mode=local --exp_name=crafter_sa_rescale_reward_no_softreset --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_rescale_reward_no_softreset.json --run_mode=local --exp_name=crafter_smin_rescale_reward_no_softreset --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_rescale_reward_no_softreset.json --run_mode=local --exp_name=crafter_smax_rescale_reward_no_softreset --training_processor_type=gpu --log_comet=true
 ############# Without normalization layer ################
 
 
@@ -27,20 +27,20 @@ sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/craft
 
 ############# With normalization layer ################
 ## SA
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_norm_layer.json --run_mode=local --exp_name=crafter_sa_norm_layer --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_rescale_reward_norm_layer.json --run_mode=local --exp_name=crafter_sa_rescale_reward_norm_layer --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_sa_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_rescale_reward_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_rescale_reward_sa_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
-## SMIN
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_norm_layer.json --run_mode=local --exp_name=crafter_smin_norm_layer --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_rescale_reward_norm_layer.json --run_mode=local --exp_name=crafter_smin_rescale_reward_norm_layer --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_smin_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_rescale_reward_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_rescale_reward_smin_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
-## SMAX
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_norm_layer.json --run_mode=local --exp_name=crafter_smax_norm_layer --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_rescale_reward_norm_layer.json --run_mode=local --exp_name=crafter_smax_rescale_reward_norm_layer --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_smax_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
-sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_rescale_reward_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_rescale_reward_smax_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_norm_layer.json --run_mode=local --exp_name=crafter_sa_norm_layer --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_rescale_reward_norm_layer.json --run_mode=local --exp_name=crafter_sa_rescale_reward_norm_layer --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_sa_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SA_rescale_reward_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_rescale_reward_sa_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
+# ## SMIN
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_norm_layer.json --run_mode=local --exp_name=crafter_smin_norm_layer --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_rescale_reward_norm_layer.json --run_mode=local --exp_name=crafter_smin_rescale_reward_norm_layer --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_smin_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMIN_rescale_reward_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_rescale_reward_smin_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
+# ## SMAX
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_norm_layer.json --run_mode=local --exp_name=crafter_smax_norm_layer --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_rescale_reward_norm_layer.json --run_mode=local --exp_name=crafter_smax_rescale_reward_norm_layer --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_smax_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
+# sbatch cluster_scripts/crafter/train scripts/dqn_smirl.py --config=configs/crafter/crafter_SMAX_rescale_reward_no_softreset_norm_layer.json --run_mode=local --exp_name=crafter_rescale_reward_smax_no_softreset_norm_layer --training_processor_type=gpu --log_comet=true
 ############# With normalization layer ################
 
 
