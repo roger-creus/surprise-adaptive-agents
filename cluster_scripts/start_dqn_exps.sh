0c78561d8634ecaed8d720c9fb5d3e72dcd3e58c
Faisal <faisal.ahmed.a.m@gmail.com>
2024-01-18 20:16:44 +0400

    wip

--- cluster_scripts/start_dqn_exps.sh
+++ cluster_scripts/start_dqn_exps.sh
@@ -1,2 +1,2 @@
-sbatch cluster_scripts/train scripts/cleanrl_dqn_crafter.py --model=smin --buffer-type=gaussian --seed=1 --train-frequency=4 --exploration-fraction=0.3 --env-id=MountainCar --batch-size=256
+sbatch cluster_scripts/train scripts/cleanrl_dqn.py --model=smin --buffer-type=gaussian --seed=1 --train-frequency=4 --exploration-fraction=0.3 --env-id=MountainCar --batch-size=256
 
