cde2bc5d2ffb311afb892867dc5257b11490bc20
Faisal <faisal.ahmed.a.m@gmail.com>
2024-01-19 11:03:26 +0400

    wip

--- scripts/cleanrl_dqn.py
+++ scripts/cleanrl_dqn.py
@@ -50,7 +50,7 @@ def parse_args():
     # Algorithm specific arguments
     parser.add_argument("--env-id", type=str, default="SurpriseAdaptRooms-v0",
         help="the id of the environment")
-    parser.add_argument("--total-timesteps", type=int, default=5000000,
+    parser.add_argument("--total-timesteps", type=int, default=10000000,
         help="total timesteps of the experiments")
     parser.add_argument("--learning-rate", type=float, default=1e-4,
         help="the learning rate of the optimizer")
