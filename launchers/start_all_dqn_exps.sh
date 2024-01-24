e898c371007321ba9c54131b54979fe7d1a9ad39
Faisal <faisal.ahmed.a.m@gmail.com>
2024-01-24 15:46:53 +0400

    wip

--- launchers/start_all_dqn_exps.sh
+++ launchers/start_all_dqn_exps.sh
@@ -7,9 +7,15 @@
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py FourRooms sadapt-inverse multinoulli 1
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py FourRooms none multinoulli 1
 
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris smin bernoulli 1 --track=True
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris smin bernoulli 8943 --track=True
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris smin bernoulli 820 --track=True
+# Tetris S-min
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris smin bernoulli 1 --track=True
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris smin bernoulli 8943 --track=True
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris smin bernoulli 820 --track=True
+
+# Tetris Extrinsic
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris none bernoulli 1 --track=True
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris none bernoulli 8943 --track=True
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris none bernoulli 820 --track=True
 
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris smax bernoulli 1
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris sadapt bernoulli 1
