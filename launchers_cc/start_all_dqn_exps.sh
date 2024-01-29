dcd30e3f7fa95a9615dc5eee19f1ba24bc019e2d
Faisal <faisal.ahmed.a.m@gmail.com>
2024-01-29 11:29:16 +0400

    wipp

--- launchers_cc/start_all_dqn_exps.sh
+++ launchers_cc/start_all_dqn_exps.sh
@@ -1,13 +1,13 @@
 # Runs to repeat
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=820 --track=True --soft_reset=1
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=1 --track=True --soft_reset=0
+sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=8943 --track=True --soft_reset=1
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=1 --track=True --soft_reset=0
 
 # # Crafter on extrinsic reward w/ soft-reset
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=1 --track=True --soft_reset=1
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=8943 --track=True --soft_reset=1
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=820 --track=True --soft_reset=1
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=1 --track=True --soft_reset=1
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=8943 --track=True --soft_reset=1
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=820 --track=True --soft_reset=1
 
-# Crafter on extrinsic reward w/o soft-reset
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=1 --track=True --soft_reset=0
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=8943 --track=True --soft_reset=0
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=820 --track=True --soft_reset=0
+# # Crafter on extrinsic reward w/o soft-reset
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=1 --track=True --soft_reset=0
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=8943 --track=True --soft_reset=0
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=820 --track=True --soft_reset=0
