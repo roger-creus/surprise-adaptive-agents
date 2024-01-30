475d146ed195bc2b17c162d79820544e3e919467
Faisal <faisal.ahmed.a.m@gmail.com>
2024-01-30 11:25:14 +0400

    wip

--- launchers_cc/start_all_dqn_exps.sh
+++ launchers_cc/start_all_dqn_exps.sh
@@ -13,9 +13,9 @@
 # sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=none --buffer-type=gaussian --seed=820 --track=True --soft_reset=0
 
 # small theta size
-# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=8943 --track=True --soft_reset=1 --theta_size="(9,9)" --wandb-project-name="Crafter_DQN_small_thetaSize"
-# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=1 --track=True --soft_reset=1 --theta_size="(9,9)" --wandb-project-name="Crafter_DQN_small_thetaSize"
-# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=820 --track=True --soft_reset=1 --theta_size="(9,9)" --wandb-project-name="Crafter_DQN_small_thetaSize"
+sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=8943 --track=True --soft_reset=1 --theta_size="(9,9)" --wandb-project-name="Crafter_DQN_small_thetaSize" --scale-by-std=0
+sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=1 --track=True --soft_reset=1 --theta_size="(9,9)" --wandb-project-name="Crafter_DQN_small_thetaSize" --scale-by-std=0
+sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=820 --track=True --soft_reset=1 --theta_size="(9,9)" --wandb-project-name="Crafter_DQN_small_thetaSize" --scale-by-std=0
 
 # sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=8943 --track=True --soft_reset=0 --theta_size="(9,9)" --wandb-project-name="Crafter_DQN_small_thetaSize"
 # sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=1 --track=True --soft_reset=0 --theta_size="(9,9)" --wandb-project-name="Crafter_DQN_small_thetaSize"
@@ -30,10 +30,10 @@
 # sbatch launchers_cc/train_cleanrl random_agent.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=820 --track=True --soft_reset=0 --theta_size="(9,9)" --wandb-project-name="Crafter_DQN_small_thetaSize"
 
 # small theta and obs size
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=8943 --track=True --soft_reset=1 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=1 --track=True --soft_reset=1 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=820 --track=True --soft_reset=1 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=8943 --track=True --soft_reset=1 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=1 --track=True --soft_reset=1 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=820 --track=True --soft_reset=1 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
 
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=8943 --track=True --soft_reset=0 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=1 --track=True --soft_reset=0 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
-sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=820 --track=True --soft_reset=0 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=8943 --track=True --soft_reset=0 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=1 --track=True --soft_reset=0 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
+# sbatch launchers_cc/train_cleanrl cleanrl_dqn.py --env-id=crafter --model=sadapt-bandit --buffer-type=gaussian --seed=820 --track=True --soft_reset=0 --theta_size="(9,9)" --obs_size="(20, 26)" --wandb-project-name="Crafter_DQN_small_thetaSize"
