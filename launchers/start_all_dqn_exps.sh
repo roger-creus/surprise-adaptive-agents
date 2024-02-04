34e8348434e2ac276a320adc412eda8699941020
Faisal <faisal.ahmed.a.m@gmail.com>
2024-02-04 12:07:11 +0400

    run experiments on butterflies

--- launchers/start_all_dqn_exps.sh
+++ launchers/start_all_dqn_exps.sh
@@ -68,25 +68,25 @@
 
 # MinAtar
 # w/o soft-reset
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smin bernoulli 820 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smin bernoulli 8943 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smin bernoulli 820 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smin bernoulli 8943 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
 
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smin bernoulli 820 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smin bernoulli 8943 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smin bernoulli 820 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smin bernoulli 8943 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
 
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smax bernoulli 820 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smax bernoulli 8943 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smax bernoulli 820 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway smax bernoulli 8943 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
 
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway sadapt-bandit bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway sadapt-bandit bernoulli 820 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Freeway sadapt-bandit bernoulli 8943 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
 
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smax bernoulli 820 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smax bernoulli 8943 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smax bernoulli 820 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+# sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smax bernoulli 8943 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
 
 # Maze
 # w/o soft-reset
@@ -110,3 +110,12 @@ sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Seaquest smax bern
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Breakout sadapt-bandit bernoulli 1 --scale-by-std=0 --soft_reset=1 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Breakout sadapt-bandit bernoulli 820 --scale-by-std=0 --soft_reset=1 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py MinAtar/Breakout sadapt-bandit bernoulli 8943 --scale-by-std=0 --soft_reset=1 --track --wandb-project-name="MinAtar_DQN" --video-log-freq=500000
+
+# Butterflies
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnv smax gaussian 1 --track --wandb-project-name="butterflies_DQN" --scale-by-std=1 --soft_reset=0 
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnv smax gaussian 820 --track --wandb-project-name="butterflies_DQN" --scale-by-std=1 --soft_reset=0
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnv smax gaussian 8943 --track --wandb-project-name="butterflies_DQN" --scale-by-std=1 --soft_reset=0
+
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnv smin gaussian 1 --track --wandb-project-name="butterflies_DQN" --scale-by-std=1 --soft_reset=0 
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnv smin gaussian 820 --track --wandb-project-name="butterflies_DQN" --scale-by-std=1 --soft_reset=0
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnv smin gaussian 8943 --track --wandb-project-name="butterflies_DQN" --scale-by-std=1 --soft_reset=0
