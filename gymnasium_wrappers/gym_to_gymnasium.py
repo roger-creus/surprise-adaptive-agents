610a5ff70ec287e6ffa843179d4165ca1f557923
Faisal <faisal.ahmed.a.m@gmail.com>
2024-02-04 10:57:11 +0400

    wip

--- gymnasium_wrappers/gym_to_gymnasium.py
+++ gymnasium_wrappers/gym_to_gymnasium.py
@@ -5,13 +5,14 @@ from gymnasium.spaces import Discrete, Box
 
 # Some envs do not support gymnasium, this simple wrapper is to convert from gym to gymnasium api
 class GymToGymnasium(gym.Env):
-    def __init__(self, env, render_mode, max_steps):
+    def __init__(self, env, render_mode, max_steps, render_kwargs=False):
         print(f"GymToGymnasium wrapper")
         self._env = env
         self._render_mode = render_mode
         self._max_steps = max_steps
         self.action_space =  Discrete(env.action_space.n)
         self.observation_space = Box(low=env.observation_space.low, high=env.observation_space.high, dtype=env.observation_space.dtype, shape=env.observation_space.shape)
+        self.render_kwargs = render_kwargs
     
     def reset(self, seed=None, options=None):
         self.t = 0
@@ -29,9 +30,8 @@ class GymToGymnasium(gym.Env):
 
     def render(self, **kwargs):
         try:
-            print("1")
-            return self._env.render(mode=self._render_mode)
-        except:
-            print("2")
             return self._env.render(**kwargs)
+        except:
+            return self._env.render(mode=self._render_mode)
+            
     
