6b7897d589df10be3cf5373c43ff215482de5941
roger <creus99@gmail.com>
2024-02-10 13:34:14 -0500

    wip:

--- launchers/start_all_paper_exps.sh
+++ launchers/start_all_paper_exps.sh
@@ -1,6 +1,45 @@
 
 # ####### DQN #######
 
+# Grafter
+sbatch launchers/train_cleanrl_roger scripts/cleanrl_dqn.py grafter none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track --video_freq=500
+sbatch launchers/train_cleanrl_roger scripts/cleanrl_dqn.py grafter none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track --video_freq=500
+sbatch launchers/train_cleanrl_roger scripts/cleanrl_dqn.py grafter none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track --video_freq=500
+
+sbatch launchers/train_cleanrl_roger scripts/cleanrl_dqn.py grafter smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --video_freq=500
+sbatch launchers/train_cleanrl_roger scripts/cleanrl_dqn.py grafter smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --video_freq=500
+sbatch launchers/train_cleanrl_roger scripts/cleanrl_dqn.py grafter smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --video_freq=500
+
+sbatch launchers/train_cleanrl_roger scripts/cleanrl_dqn.py grafter smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --video_freq=500
+sbatch launchers/train_cleanrl_roger scripts/cleanrl_dqn.py grafter smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --video_freq=500
+sbatch launchers/train_cleanrl_roger scripts/cleanrl_dqn.py grafter smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --video_freq=500
+
+# MazeEnvLarge
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-MazeEnvLarge none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-MazeEnvLarge none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-MazeEnvLarge none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-MazeEnvLarge smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-MazeEnvLarge smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-MazeEnvLarge smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-MazeEnvLarge smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-MazeEnvLarge smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-MazeEnvLarge smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+
+# ButterfliesEnvLarge
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+sbatch launchers/train_cleanrl_roger_long scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000
+
 # # Tetris
 # sbatch launchers/train_cleanrl_roger scripts/cleanrl_dqn.py tetris none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track
 # sbatch launchers/train_cleanrl_roger scripts/cleanrl_dqn.py tetris none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track
@@ -68,66 +107,66 @@
 
 # ####### PPO #######
 # Tetris
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track
 
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
 
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py tetris smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
 
-# Maze
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track
+# # Maze
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track
 
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
 
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-MazeEnv smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
 
-# Butterflies
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track
+# # Butterflies
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track
 
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
 
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py griddly-ButterfliesEnv smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
 
-# MinAtar/Breakout
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track
+# # MinAtar/Breakout
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track
 
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
 
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/Breakout smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
 
-# MinAtar/SpaceInvaders
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track
+# # MinAtar/SpaceInvaders
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders none bernoulli 1 --scale-by-std=0 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders none bernoulli 2 --scale-by-std=0 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders none bernoulli 3 --scale-by-std=0 --soft_reset=0 --track
 
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smin bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smin bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smin bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
 
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
-sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smax bernoulli 1 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smax bernoulli 2 --scale-by-std=1 --soft_reset=0 --track
+# sbatch launchers/train_cleanrl_roger scripts/cleanrl_ppo.py MinAtar/SpaceInvaders smax bernoulli 3 --scale-by-std=1 --soft_reset=0 --track
