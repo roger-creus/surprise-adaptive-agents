dc512ae6517d47480ba74503ef83bce61a338bbc
Faisal <faisal.ahmed.a.m@gmail.com>
2024-02-12 15:55:31 +0400

    wip

--- launchers/start_all_paper_exps_sadapt.sh
+++ launchers/start_all_paper_exps_sadapt.sh
@@ -7,19 +7,19 @@
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py grafter sadapt-bandit bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --video_freq=500
 
 # MazeEnvLarge
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-MazeEnvLarge sadapt-bandit bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit"
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-MazeEnvLarge sadapt-bandit bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit"
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-MazeEnvLarge sadapt-bandit bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit"
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-MazeEnvLarge sadapt-bandit bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit" --ucb_coeff=1
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-MazeEnvLarge sadapt-bandit bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit" --ucb_coeff=1
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-MazeEnvLarge sadapt-bandit bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit" --ucb_coeff=1
 
 # ButterfliesEnvLarge
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge sadapt-bandit bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit"
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge sadapt-bandit bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit"
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge sadapt-bandit bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit"
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge sadapt-bandit bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit" --ucb_coeff=1
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge sadapt-bandit bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit" --ucb_coeff=1
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-ButterfliesEnvLarge sadapt-bandit bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --video_freq=500 --buffer-size=100_000 --wandb-project-name="sadapt-bandit" --ucb_coeff=1
 
 # # Tetris
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris sadapt-bandit bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="sadapt-bandit"
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris sadapt-bandit bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="sadapt-bandit"
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris sadapt-bandit bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="sadapt-bandit"
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris sadapt-bandit bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="sadapt-bandit" --ucb_coeff=1
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris sadapt-bandit bernoulli 2 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="sadapt-bandit" --ucb_coeff=1
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py tetris sadapt-bandit bernoulli 3 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="sadapt-bandit" --ucb_coeff=1
 
 # # Maze
 # sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py griddly-MazeEnv sadapt-bandit bernoulli 1 --scale-by-std=1 --soft_reset=0 --track --wandb-project-name="sadapt-bandit"
