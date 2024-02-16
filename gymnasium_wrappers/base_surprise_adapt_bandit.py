347d0e298e3dd6bc643f6a9030cb514180940ffa
Faisal <faisal.ahmed.a.m@gmail.com>
2024-02-16 17:19:59 +0400

    use seprate channel fot t and alpha+cleaning

--- gymnasium_wrappers/base_surprise_adapt_bandit.py
+++ gymnasium_wrappers/base_surprise_adapt_bandit.py
@@ -4,8 +4,6 @@ from gymnasium.spaces import Box, Dict
 import cv2
 from IPython import embed
 
-# This is taken from the bandit_final branch
-
 class BaseSurpriseAdaptBanditWrapper(gym.Env):
     def __init__(
         self,
@@ -20,21 +18,23 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
         soft_reset=True,
         ucb_coeff=np.sqrt(2),
         death_cost = False,
-        exp_rew = False
+        exp_rew = False,
     ):
-        """
+        '''
         params
         ======
         env (gym.Env) : environment to wrap
 
         buffer (Buffer object) : Buffer that tracks history and fits models
-        """
+        '''
+
         self._env = env
         self.buffer = buffer
-        self.max_steps = max_steps
-        self.add_true_rew = add_true_rew
-        self.int_rew_scale = int_rew_scale
-        self.ext_only = ext_only
+        self._theta_size = theta_size
+        self._grayscale = grayscale
+        self._soft_reset = soft_reset
+        self._death_cost = death_cost
+        self._exp_rew = exp_rew
         self._theta_size = theta_size
         self._grayscale = grayscale
         self._soft_reset = soft_reset
@@ -42,55 +42,53 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
         self._death_cost = death_cost
         self._exp_rew = exp_rew
 
-        print(f"_theta_size:{self._theta_size}")
-        print(f"_grayscale:{self._grayscale}")
-
         theta = self.buffer.get_params()
-
         print(f"theta shape:{theta.shape}")
-        
+
+        # Add true reward to surprise
+        self.add_true_rew = add_true_rew
+        self.int_rew_scale = int_rew_scale
         self.num_steps = 0
         self.num_eps = 0
+        self.ext_only = ext_only
+        self.max_steps = max_steps
         self.deaths = 0
-        self.alpha_rolling_average = 0
-        self.alpha_count = 1
-
+        self.alpha_rolling_average = np.nan
+        self.alpha_count = 0
+        
+        
         # Gym spaces
         self.action_space = env.action_space
         self.env_obs_space = env.observation_space
-
+        
         # the new theta shape has to be the extact theta.shape but +2 in first dimension
         # the additional dimension are the time-step and bandit choice
-        new_theta_shape = (theta.shape[0] + 2, )
-        for i in range(len(theta.shape) - 1):
-            new_theta_shape += (theta.shape[i+1],)
-
-        # the new obs shape has to be the extact obs.shape but +1 in first dimension
-        # the additional dimension is the bandit choice
-        obs, _ = env.reset()
-        obs_shape = obs["obs"].shape if isinstance(obs, dict) else obs.shape
-        new_obs_shape = (obs_shape[0] + 1, )
-        for i in range(len(obs_shape) - 1):
-            new_obs_shape += (obs_shape[i+1],)
-
-        print(f"new_obs_shape:{new_obs_shape}")
-        print(f"new_theta_shape:{new_theta_shape}")
+        new_theta_shape = (theta.shape[0], )
+        for i in range(1, len(theta.shape)):
+            if i == len(theta.shape)-1:
+                new_theta_shape += (theta.shape[i] + 2, ) # in the last index (the channel dim) add 2 channels
+            else:
+                new_theta_shape += (theta.shape[i], )
+        print(f"new_theta_shape: {new_theta_shape}")
 
         # instead of hardcoding the keys. Make sure to add all the keys from the original observation space
         obs_space = {}
         if isinstance(self.env_obs_space, Box):
-            obs_space["obs"] = Box(0, 1, shape=new_obs_shape)
+            obs_space["obs"] = self.env_obs_space
         elif isinstance(self.env_obs_space, Dict):
             for key in self.env_obs_space.spaces.keys():
                 obs_space[key] = self.env_obs_space.spaces[key]
-            obs_space["obs"] = Box(0, 1, shape=new_obs_shape)
         else:
             raise ValueError("Observation space not supported")
 
         obs_space["theta"] = Box(-np.inf, np.inf, shape=new_theta_shape)
         self.observation_space = Dict(obs_space)
-        print(f"observation_space:{self.observation_space}")
-            
+
+        try:
+            self.heatmap = np.zeros((env.width, env.height))
+        except:
+            self.heatmap = None
+
         # Bandit statistics
         self.alpha_zero_mean = np.nan
         self.alpha_zero_cnt = 0
@@ -100,41 +98,30 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
         self.ucb_alpha_zero = None
         self.ucb_alpha_one = None
         self.alpha_t = np.nan
-        try:
-            self.heatmap = np.zeros((env.width, env.height))
-        except:
-            self.heatmap = None
-
-        self.reset()
-
-    def set_alpha_zero_mean(self, val):
-        self.alpha_zero_mean = val
-    
-    def set_alpha_one_mean(self, val):
-        self.alpha_one_mean = val
+            
+        print(self.observation_space)
 
     def _get_random_entropy(self):
-        obs, info = self._env.reset()
+        obs, _ = self._env.reset()
         self.buffer.reset()
-        # number of episodes to evaluate the random entorpy
-        num_eps = 10
+        num_eps = 100
         random_entropies = []
         for _ in range(num_eps):
-            for _ in range(self.max_steps):
-                obs, rew, envdone, envdtrunc, info = self._env.step(self.action_space.sample())
+            for t in range(self.max_steps):
+                obs, rew, envdone, envtrunc, info = self._env.step(self.action_space.sample())
                 self.buffer.add(self.encode_obs(obs))
-                if envdone or envdtrunc:
+                if envdone or envtrunc:
                     if self._soft_reset:
                         obs, _ = self._env.reset()
-                        self.buffer.add(self.encode_obs(obs))
                     else:
                         break
             random_entropy = self.buffer.entropy()
             random_entropies.append(random_entropy)
             obs, _ = self._env.reset()
             self.buffer.reset()
-        random_entropy = np.mean(random_entropies)
-        return random_entropy
+        print(f"len(random_entropies): {len(random_entropies)}")
+        self.buffer.reset()
+        return np.mean(random_entropies)
 
     def _get_alpha(self):
         ucb_alpha_zero = None
@@ -147,8 +134,8 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
             elif self.alpha_one_cnt == 0:
                 alpha_t = 1
             else:
-                ucb_alpha_zero = self.ucb_coeff * np.sqrt(np.log(self.num_eps) / self.alpha_zero_cnt)
-                ucb_alpha_one =  self.ucb_coeff * np.sqrt(np.log(self.num_eps) / self.alpha_one_cnt)
+                ucb_alpha_zero = np.sqrt(2 * np.log(self.num_eps) / self.alpha_zero_cnt)
+                ucb_alpha_one =  np.sqrt(2 * np.log(self.num_eps) / self.alpha_one_cnt)
                 alpha_t = np.argmax(
                     [
                         self.alpha_zero_mean
@@ -158,22 +145,21 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
                     ]
                 )
         return alpha_t, ucb_alpha_zero, ucb_alpha_one
-
+    
     def step(self, action):
-        # Take Action
         obs, env_rew, envdone, envtrunc, info = self._env.step(action)
+        info['task_reward'] = env_rew
         self.task_return += env_rew
-        info["task_reward"] = env_rew
         info["alpha"] = self.alpha_t
         info["alpha_zero_mean"] = self.alpha_zero_mean
         info["alpha_zero_cnt"] = self.alpha_zero_cnt
         info["alpha_one_mean"] = self.alpha_one_mean
         info["alpha_one_cnt"] = self.alpha_one_cnt
         info["random_entropy"] = self.random_entropy
-
+        
         # soft reset
         if self._soft_reset:
-            if envdone or envtrunc:
+            if envdone:
                 obs, _ = self._env.reset()
                 # obs = np.random.rand(*obs.shape)
                 self.deaths += 1
@@ -185,6 +171,7 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
                     self.num_steps /= self.deaths
                 info["Average_task_return"] = self.task_return
                 info["Average_episode_length"] = self.num_steps
+                info['deaths'] = self.deaths
                 info["alpha_rolling_average"] = self.alpha_rolling_average
                 if self.ucb_alpha_one: info["ucb_alpha_one"] = self.ucb_alpha_one
                 if self.ucb_alpha_zero: info["ucb_alpha_zero"] = self.ucb_alpha_zero
@@ -195,30 +182,29 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
             if envdone or envtrunc:
                 info["Average_task_return"] = self.task_return
                 info["Average_episode_length"] = self.num_steps
+                info['deaths'] = self.deaths
                 info["alpha_rolling_average"] = self.alpha_rolling_average
                 if self.ucb_alpha_one: info["ucb_alpha_one"] = self.ucb_alpha_one
                 if self.ucb_alpha_zero: info["ucb_alpha_zero"] = self.ucb_alpha_zero
 
-        
-        # Compute surprise as the negative log probability of the observation
+        # use the original observation for surprise calculation
+        # this will be used for griddly envs and compute surprise with the bernoulli buffer
         surprise = -self.buffer.logprob(self.encode_obs(obs))
         thresh = 300
         surprise = np.clip(surprise, a_min=-thresh, a_max=thresh) / thresh
+        
 
+        self.buffer.add(self.encode_obs(obs))
+        info['surprise'] = surprise
+        info["theta_entropy"] = self.buffer.entropy()
+        info['deaths'] = self.deaths
+        
+        # Add observation to buffer
         if self._exp_rew:
             surprise = np.exp(surprise)
 
         rew = ((-1) ** self.alpha_t) * surprise
         
-
-        # Add observation to buffer
-        self.buffer.add(self.encode_obs(obs))
-
-        info["surprise_adapt_reward"] = rew
-        info["theta_entropy"] = self.buffer.entropy()
-        info['deaths'] = self.deaths
-        info["surprise"] = surprise
-        info["alpha"] = self.alpha_t
         
         if self.add_true_rew:
             rew = env_rew + (rew * self.int_rew_scale)
@@ -226,14 +212,12 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
             rew = rew * self.int_rew_scale
             
         if self.ext_only:
-            rew = env_rew        
+            rew = env_rew
 
         if self._death_cost and (envdone or envtrunc):
             rew = -100
-
-        # augment next state
-        obs = self.get_obs(obs)
-
+            
+                
         try:
             x, y = self._env.agent_pos
             self.heatmap[x, y] += 1
@@ -242,13 +226,12 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
             pass
 
         self.num_steps += 1
-
-        return obs, rew, envdone, envtrunc, info
+        return self.get_obs(obs), rew, envdone, envtrunc, info
 
     def get_obs(self, obs):
-        """
-        Augment observation, perhaps with generative model params, time-step, current surprise momentum.
-        """
+        '''
+        Augment observation, perhaps with generative model params
+        '''
         theta = self.buffer.get_params()
         num_samples = (np.ones(1)*self.buffer.buffer_size) / self.max_steps
 
@@ -261,25 +244,28 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
         else:
             raise ValueError("Observation space not supported")
         
-        num_samples = (np.ones_like(theta[0]) * num_samples)
-        alpha_t = np.ones_like(theta[0]) * self.alpha_t
+        num_samples = (np.ones(theta.shape[:-1]) * num_samples)[..., None]
+        alpha_t = (np.ones(theta.shape[:-1]) * self.alpha_t)[..., None]
 
-        aug_obs["theta"] = np.concatenate([theta, 
-                                        num_samples[None, :],
-                                        alpha_t[None, :]], axis=0)
-        aug_obs["obs"] = np.concatenate([aug_obs["obs"], alpha_t[None, :]], axis=0)
+        theta_obs = np.concatenate([theta,
+                                    num_samples,
+                                    alpha_t], axis=-1)
+        
+        # print(f"theta shape before cat: {theta.shape}")
+        # print(f"theta shape after cat: {theta_obs.shape}")
+
+        aug_obs["theta"] = theta_obs
+        
         return aug_obs
 
-    def reset(self,  seed=None, options=None):
-        """
-        Reset the wrapped env and the buffer
-        """
-
+    def reset(self, seed=None, options=None):
         obs, info = self._env.reset()
-        self.task_return = 0
+        self.buffer.reset()
+        self.num_steps = 0
         self.deaths = 0
+        self.task_return = 0
 
-        # Update the bandit action values
+        # update the bandit statistics
         if not np.isnan(self.alpha_t):
             entropy_change = (
                 np.abs(self.buffer.entropy() - self.random_entropy)
@@ -290,39 +276,38 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
                     self.alpha_zero_mean = entropy_change
                     self.alpha_zero_cnt += 1
                 else:
+                    self.alpha_zero_cnt += 1
                     self.alpha_zero_mean = (
                         self.alpha_zero_mean * self.alpha_zero_cnt + entropy_change
                     )
-                    self.alpha_zero_cnt += 1
                     self.alpha_zero_mean /= self.alpha_zero_cnt
             else:
                 if np.isnan(self.alpha_one_mean):
                     self.alpha_one_mean = entropy_change
                     self.alpha_one_cnt += 1
                 else:
+                    self.alpha_one_cnt += 1
                     self.alpha_one_mean = (
                         self.alpha_one_mean * self.alpha_one_cnt + entropy_change
                     )
-                    self.alpha_one_cnt += 1
+                    
                     self.alpha_one_mean /= self.alpha_one_cnt
 
         # select new alpha
         self.alpha_t,  self.ucb_alpha_zero, self.ucb_alpha_one = self._get_alpha()
         # track the rolling average of alpha
-        self.alpha_rolling_average = self.alpha_rolling_average + (1/self.alpha_count) * (self.alpha_t - self.alpha_rolling_average)
-        self.alpha_count += 1
+        if np.isnan(self.alpha_rolling_average):
+            self.alpha_rolling_average = self.alpha_t
+            self.alpha_count += 1
+        else:
+            self.alpha_rolling_average = self.alpha_rolling_average + (1/self.alpha_count) * (self.alpha_t - self.alpha_rolling_average)
+            self.alpha_count += 1
 
-        # log the entropy change
-        # print(f"Agent entropy: {self.buffer.entropy()}")
-        # print(f"Random entropy: {self.random_entropy}")
         info["entropy_change"] = (self.buffer.entropy() - self.random_entropy)
-
-        self.buffer.reset()
 
         self.num_eps += 1
         self.num_steps = 0
-
-        obs = self.get_obs(obs)
+        
         if self.heatmap is not None:
             info["heatmap"] = self.heatmap.copy()
         try:
@@ -330,6 +315,8 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
         except:
             self.heatmap = None
         
+        self.buffer.reset()
+        obs = self.get_obs(obs)
         return obs, info
 
     def render(self, **kwargs):
@@ -352,4 +339,3 @@ class BaseSurpriseAdaptBanditWrapper(gym.Env):
             return obs["obs"].astype(np.float32)
         else:
             return obs.astype(np.float32)
-        
