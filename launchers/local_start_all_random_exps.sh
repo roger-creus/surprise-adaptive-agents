f758c3c8d8a78c68ee39ff245b9935ad482a9a69
roger-creus <creus99@gmail.com>
2024-02-21 11:23:02 -0500

    wip

--- launchers/local_start_all_random_exps.sh
+++ launchers/local_start_all_random_exps.sh
@@ -1,62 +1,61 @@
-
-
 # # MazeEnvLarge
-python scripts/random_agent.py griddly-MazeEnvLarge none bernoulli 1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-MazeEnvLarge none bernoulli 2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-MazeEnvLarge none bernoulli 3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 
+python scripts/random_agent.py --env-id=griddly-MazeEnvLarge --model=none --buffer-type=bernoulli --seed=1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-MazeEnvLarge --model=none --buffer-type=bernoulli --seed=2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-MazeEnvLarge --model=none --buffer-type=bernoulli --seed=3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 
 wait
 
 # # # ButterfliesEnvLarge
-python scripts/random_agent.py griddly-ButterfliesEnvLarge none bernoulli 1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-ButterfliesEnvLarge none bernoulli 2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-ButterfliesEnvLarge none bernoulli 3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 
+python scripts/random_agent.py --env-id=griddly-ButterfliesEnvLarge --model=none --buffer-type=bernoulli --seed=1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-ButterfliesEnvLarge --model=none --buffer-type=bernoulli --seed=2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-ButterfliesEnvLarge --model=none --buffer-type=bernoulli --seed=3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 
 wait
 
-# # Tetris
-python scripts/random_agent.py tetris none bernoulli 1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py tetris none bernoulli 2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py tetris none bernoulli 3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 
+# tetris
+python scripts/random_agent.py --env-id=tetris --model=none --buffer-type=bernoulli --seed=1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=tetris --model=none --buffer-type=bernoulli --seed=2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=tetris --model=none --buffer-type=bernoulli --seed=3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 
 wait
 
 # # Maze
-python scripts/random_agent.py griddly-MazeEnv none bernoulli 1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-MazeEnv none bernoulli 2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-MazeEnv none bernoulli 3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 
+python scripts/random_agent.py --env-id=griddly-MazeEnv --model=none --buffer-type=bernoulli --seed=1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-MazeEnv --model=none --buffer-type=bernoulli --seed=2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-MazeEnv --model=none --buffer-type=bernoulli --seed=3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 
 wait
 
 # # Butterflies
-python scripts/random_agent.py griddly-ButterfliesEnv none bernoulli 1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-ButterfliesEnv none bernoulli 2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-ButterfliesEnv none bernoulli 3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000
+python scripts/random_agent.py --env-id=griddly-ButterfliesEnv --model=none --buffer-type=bernoulli --seed=1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-ButterfliesEnv --model=none --buffer-type=bernoulli --seed=2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-ButterfliesEnv --model=none --buffer-type=bernoulli --seed=3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100
 wait
 
 # MazeEnvLarge2
-python scripts/random_agent.py griddly-MazeEnvLarge2 none bernoulli 1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-MazeEnvLarge2 none bernoulli 2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-MazeEnvLarge2 none bernoulli 3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000
+python scripts/random_agent.py --env-id=griddly-MazeEnvLarge2 --model=none --buffer-type=bernoulli --seed=1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-MazeEnvLarge2 --model=none --buffer-type=bernoulli --seed=2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-MazeEnvLarge2 --model=none --buffer-type=bernoulli --seed=3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100
 wait
 
 # Maze2
-python scripts/random_agent.py griddly-MazeEnv2 none bernoulli 1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-MazeEnv2 none bernoulli 2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-MazeEnv2 none bernoulli 3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000
+python scripts/random_agent.py --env-id=griddly-MazeEnv2 --model=none --buffer-type=bernoulli --seed=1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-MazeEnv2 --model=none --buffer-type=bernoulli --seed=2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-MazeEnv2 --model=none --buffer-type=bernoulli --seed=3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100
 wait
 
 # # ButterfliesEnvLarge2
-python scripts/random_agent.py griddly-ButterfliesEnvLarge2 none bernoulli 1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-ButterfliesEnvLarge2 none bernoulli 2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py griddly-ButterfliesEnvLarge2 none bernoulli 3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000
+python scripts/random_agent.py --env-id=griddly-ButterfliesEnvLarge2 --model=none --buffer-type=bernoulli --seed=1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-ButterfliesEnvLarge2 --model=none --buffer-type=bernoulli --seed=2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=griddly-ButterfliesEnvLarge2 --model=none --buffer-type=bernoulli --seed=3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100
 wait
 
 ######## MINATAR ########
 
 # MinAtar/Breakout
-python scripts/random_agent.py MinAtar/Breakout none bernoulli 1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py MinAtar/Breakout none bernoulli 2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py MinAtar/Breakout none bernoulli 3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000
+python scripts/random_agent.py --env-id=MinAtar/Breakout --model=none --buffer-type=bernoulli --seed=1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=MinAtar/Breakout --model=none --buffer-type=bernoulli --seed=2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=MinAtar/Breakout --model=none --buffer-type=bernoulli --seed=3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100
 wait
 
-# # MinAtar/SpaceInvaders
-python scripts/random_agent.py MinAtar/SpaceInvaders none bernoulli 1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py MinAtar/SpaceInvaders none bernoulli 2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 &
-python scripts/random_agent.py MinAtar/SpaceInvaders none bernoulli 3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000
+# # --env-id=MinAtar/SpaceInvaders
+python scripts/random_agent.py --env-id=MinAtar/SpaceInvaders --model=none --buffer-type=bernoulli --seed=1 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=MinAtar/SpaceInvaders --model=none --buffer-type=bernoulli --seed=2 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100 &
+python scripts/random_agent.py --env-id=MinAtar/SpaceInvaders --model=none --buffer-type=bernoulli --seed=3 --scale-by-std=0 --soft_reset=0 --total-timesteps=10_000 --buffer-size=100
+wait
