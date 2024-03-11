a3dd5bb487da195abd0ea5d728c855d42a278309
Faisal <faisal.ahmed.a.m@gmail.com>
2024-03-11 13:59:35 +0400

    wip

--- launchers/start_atari_exps.sh
+++ launchers/start_atari_exps.sh
@@ -1,22 +1,26 @@
 # Freeway
 # Extrinsic 
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway none gaussian 1 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway none gaussian 2 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway none gaussian 3 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway none gaussian 1 --scale-by-std=0 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway none gaussian 2 --scale-by-std=0 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway none gaussian 3 --scale-by-std=0 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway none gaussian 1 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway none gaussian 2 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway none gaussian 3 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
 
 # Sadapt
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway sadapt-bandit gaussian 1 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway sadapt-bandit gaussian 2 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway sadapt-bandit gaussian 3 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway sadapt-bandit gaussian 1 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway sadapt-bandit gaussian 2 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway sadapt-bandit gaussian 3 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
 
 # SMax
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smax gaussian 1 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smax gaussian 2 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smax gaussian 3 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smax gaussian 1 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smax gaussian 2 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smax gaussian 3 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
 
 # SMin
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smin gaussian 1 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smin gaussian 2 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
-sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smin gaussian 3 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.3 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smin gaussian 1 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smin gaussian 2 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
+sbatch launchers/train_cleanrl scripts/cleanrl_dqn.py Atari-Freeway smin gaussian 3 --scale-by-std=1 --soft_reset=0 --track --theta_size="(32,32)" --exploration-fraction=0.1 --wandb-project-name="sadapt_bandit_atari" --total-timesteps=50_000_000
 
 
